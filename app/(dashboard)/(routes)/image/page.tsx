0c2034838838e5e58fc745caac87cd863f44b4dc
Opemipo20201 <omoimole2020@gmail.com>
2023-10-23 11:34:15 +0100

    Initial Commit

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import * as z from "zod";
+import { Heading } from "@/components/heading";
+
+import { ImageIcon, MessageSquare, Music } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/user-avatar";
+import { BotAvatar } from "@/components/bot-avatar";
+
+const MusicPage = () => {
+    const router = useRouter();
+    const [images, setImages] = useState<string[]>([]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ""
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post("/api/conversation");
+
+            form.reset();
+        } catch (error: any) {
+            // TODO: Open Pro Model
+            console.log(error)
+        } finally {
+            router.refresh();
+        }
+    };
+
+    return (
+        <div>
+            <Heading
+                title="Image Generation"
+                description="Turn your prompts into an image with Einstein."
+                icon={ImageIcon}
+                iconColor="text-pink-700"
+                bgColor="bg-pink-700/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="How do I calculate the radius of a circle"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-20">
+                            <Loader />
+                        </div>
+                    )}
+                    {images.length === 0 && !isLoading && (
+                        <div>
+                            <Empty label="No Images generated." />
+                        </div>
+                    )}
+                    <div>
+                        Images will be rendered here soon.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MusicPage;
